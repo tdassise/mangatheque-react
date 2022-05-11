7f01358332feedf60dad83f4ba034aaf9b674601
tdassise <tibo.d37@gmail.com>
2022-05-11 14:59:28 +0200

    Ajout page login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(e.target.email.value);
+
+    if (!e.target.email.value) {
+      alert("Email is required");
+    } else if (!e.target.email.value) {
+      alert("Valid email is required");
+    } else if (!e.target.password.value) {
+      alert("Password is required");
+    } else if (
+        e.target.email.value === "me@example.com" &&
+        e.target.password.value === "123456"
+    ) {
+      alert("Successfully logged in");
+      e.target.email.value = "";
+      e.target.password.value = "";
+    } else {
+      alert("Wrong email or password combination");
+    }
+  };
+
+  handleClick = e => {
+    e.preventDefault();
+
+    alert("Goes to registration page");
+  };
+
+  render() {
+    return (
+        <div className="App">
+          <img src={logo} className="logo" alt="Business view - Reports" />
+          <form className="form" onSubmit={this.handleSubmit}>
+            <div className="input-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" placeholder="nome@email.com.br" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Mot de passe</label>
+              <input type="password" name="password" />
+            </div>
+            <button className="primary">Se connecter</button>
+          </form>
+          <button className="secondary" onClick={this.handleClick}>
+            S'inscrire
+          </button>
+        </div>
+    );
+  }
 }
 
 export default App;
+
